de3cbd06bd8defcff94bd0b8225da9142fbf357b
teddycode <dcl_gx@163.com>
2025-06-04 11:33:33 +0800

    doc: add benchmark  scripts

--- scripts/faulty_leader_demo.sh
+++ scripts/faulty_leader_demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 killall hotstuff-app
-./examples/hotstuff-app --conf ./hotstuff-sec0.conf > log0 2>&1 &
+./examples/hotstuff-app --conf ./hotstuff-sec0.conf > log0.log 2>&1 &
 leader_pid="$!"
 rep=({1..3})
 if [[ $# -gt 0 ]]; then
@@ -10,7 +10,7 @@ for i in "${rep[@]}"; do
     echo "starting replica $i"
     #valgrind --leak-check=full ./examples/hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
     #gdb -ex r -ex bt -ex q --args ./examples/hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
-    ./examples/hotstuff-app --conf ./hotstuff-sec${i}.conf > log${i} 2>&1 &
+    ./examples/hotstuff-app --conf ./hotstuff-sec${i}.conf > log${i}.log 2>&1 &
 done
 echo "All replicas started. Let's issue some commands to be replicated (in 5 sec)..."
 sleep 5
@@ -21,4 +21,4 @@ sleep 5
 kill "$leader_pid"
 echo "Leader is dead. Let's try to restart our clients (because the simple clients don't timeout/retry some lost requests)."
 kill "$cli_pid"
-./examples/hotstuff-client --idx 0 --iter -1 --max-async 4
+./examples/hotstuff-client --idx 0 --iter -1 --max-async 4 > log_client.log 2>&1
